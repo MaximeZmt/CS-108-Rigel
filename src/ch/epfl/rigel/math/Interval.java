d8fc67233f28220ddea828dcec676634abc68098
Naarileth <Naarileth@users.noreply.github.com>
2020-02-18 12:47:54 +0100

    doc interval

--- src/ch/epfl/rigel/math/Interval.java
+++ src/ch/epfl/rigel/math/Interval.java
@@ -1,27 +1,57 @@
 package ch.epfl.rigel.math;
 
+/**
+ * Represents a basic interval
+ */
 public abstract class Interval {
 
     private final double a;
     private final double b;
 
+    /**
+     * Builds a basic interval
+     *
+     * @param a  the lower bound
+     * @param b  the upper bound
+     */
     protected Interval(double a, double b) {
         this.a = a;
         this.b = b;
     }
 
+    /**
+     * Getter for the lower bound
+     *
+     * @return the lowest number
+     */
     public double low() {
         return a;
     }
 
+    /**
+     * Getter for the upper bound
+     *
+     * @return the highest number
+     */
     public double high() {
         return b;
     }
 
+    /**
+     * Getter for the size of the interval
+     *
+     * @return the size of the interval
+     */
     public double size() {
         return b-a;
     }
 
+    /**
+     * Checks if a value is contained in the interval
+     *
+     * @param v  the value to be checked
+     * @return a boolean
+     */
     public abstract boolean contains (double v);
 
     @Override
