dffb9b576144f1c78995d67d99aeb7652fb864c1
Naarileth <Naarileth@users.noreply.github.com>
2020-02-18 12:35:07 +0100

    closed interval doc

--- src/ch/epfl/rigel/math/ClosedInterval.java
+++ src/ch/epfl/rigel/math/ClosedInterval.java
@@ -21,21 +21,34 @@ public final class ClosedInterval extends Interval {
 
     /**
      * Builds a closed interval
+     * checks if low<high
      *
-     * @param low
-     * @param high
-     * @return
+     * @param low  lowest number
+     * @param high  highest number
+     * @return a closed interval
      */
     public static ClosedInterval of(double low, double high) {
         Preconditions.checkArgument(low<high);
         return new ClosedInterval(low, high);
     }
 
+    /**
+     * Builds a symmetric closed interval centered in 0
+     *
+     * @param size  size of the interval
+     * @return a closed interval
+     */
     public static ClosedInterval symmetric(double size) {
         Preconditions.checkArgument(size>0);
         return new ClosedInterval(-size/2, size/2);
     }
 
+    /**
+     * Clips a value into the interval
+     *
+     * @param v  value to be clipped
+     * @return the clipped value
+     */
     public double clip(double v) { //TODO check access right and done: Ecretage
         if(v<= low()) {
             return low();
