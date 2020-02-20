e192493dc91a35356b320ea5103e4752cef545d9
Naarileth <Naarileth@users.noreply.github.com>
2020-02-20 17:03:02 +0100

    geographic coordinates + test

--- src/ch/epfl/rigel/coordinates/GeographicCoordinates.java
+++ src/ch/epfl/rigel/coordinates/GeographicCoordinates.java
@@ -5,20 +5,63 @@ import ch.epfl.rigel.math.Angle;
 import ch.epfl.rigel.math.ClosedInterval;
 import ch.epfl.rigel.math.RightOpenInterval;
 
+import java.util.Locale;
+
 public final class GeographicCoordinates extends SphericalCoordinates {
 
     private final static RightOpenInterval LON_INTERVAL = RightOpenInterval.of(-180, 180);
     private final static ClosedInterval LAT_INTERVAL = ClosedInterval.of(-90,90);
-
 
     private GeographicCoordinates(double longitude, double latitude) {
         super(longitude, latitude);
     }
 
     public static GeographicCoordinates ofDeg(double lonDeg, double latDeg){
-        ClosedInterval latInterval = ClosedInterval.of(-90,90);
         Preconditions.checkInInterval(LON_INTERVAL, lonDeg);
         Preconditions.checkInInterval(LAT_INTERVAL, latDeg);
         return new GeographicCoordinates(Angle.ofDeg(lonDeg), Angle.ofDeg(latDeg));
+    }
+
+    public static boolean isValidLonDeg(double lonDeg){
+        try {
+            Preconditions.checkInInterval(LON_INTERVAL, lonDeg);
+        } catch (IllegalArgumentException e){
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean isValidLatDeg(double latDeg){
+        try {
+            Preconditions.checkInInterval(LAT_INTERVAL, latDeg);
+        } catch (IllegalArgumentException e){
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public double lon() {
+        return super.lon();
+    }
+
+    @Override
+    public double lonDeg() {
+        return super.lonDeg();
+    }
+
+    @Override
+    public double lat() {
+        return super.lat();
+    }
+
+    @Override
+    public double latDeg() {
+        return super.latDeg();
+    }
+
+    @Override
+    public String toString() {
+        return String.format(Locale.ROOT, "(lon=%.4f°, lat=%.4f°)", lonDeg(), latDeg());
     }
 }
---
+++ test/ch/epfl/rigel/coordinates/GeographicCoordinatesTest.java
@@ -0,0 +1,18 @@
+package ch.epfl.rigel.coordinates;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GeographicCoordinatesTest {
+
+
+    @Test
+    void toStringWorksOnRandomCoordinates(){
+        GeographicCoordinates gc = GeographicCoordinates.ofDeg(45.34567,27.654);
+        assertEquals("(lon=45.3457°, lat=27.6540°)", gc.toString());
+    }
+
+
+
+}
