83df0aea59474499accad37402135010fbf7e5c6
maxime.zammit <maxime.zammit@epfl.ch>
2020-02-21 13:35:17 +0100

    Update Polynomial.java

--- src/ch/epfl/rigel/math/Polynomial.java
+++ src/ch/epfl/rigel/math/Polynomial.java
@@ -44,7 +44,7 @@ public final class Polynomial {
     }
 
     @Override
-    public String toString() { 
+    public String toString() {
         StringBuilder sb = new StringBuilder();
         for (int i=0 ; i<coefficients.length ; ++i){
             if(coefficients[i]!=0){
