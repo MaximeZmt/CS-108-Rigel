e6a184366b8e9233c768dddc6e0cff2b42c5900b
Naarileth <Naarileth@users.noreply.github.com>
2020-02-24 13:26:45 +0100

    Update Epoch.java

--- src/ch/epfl/rigel/astronomy/Epoch.java
+++ src/ch/epfl/rigel/astronomy/Epoch.java
@@ -6,5 +6,11 @@ public enum Epoch {
     J2000,
     J2010;
 
-    public double daysUntil(ZonedDateTime when)
+    public double daysUntil(ZonedDateTime when){
+        return 0;
+    }
+
+    public double julianCenturiesUntil(ZonedDateTime when){
+        return 0;
+    }
 }
