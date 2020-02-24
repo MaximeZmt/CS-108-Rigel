b855fb1ac2ced927df3b83089710b09382549667
Naarileth <Naarileth@users.noreply.github.com>
2020-02-24 19:32:16 +0100

    push

---
+++ src/ch/epfl/rigel/coordinates/EquatorialToHorizontalConversion.java
@@ -0,0 +1,11 @@
+package ch.epfl.rigel.coordinates;
+
+import java.time.*; //TODO ask if import * or just what we need
+
+// représente un changement de systèmes de coordonnées
+// depuis les coordonnées équatoriales vers les coordonnées écliptiques, à un instant et pour un lieu donnés.
+public final class EquatorialToHorizontalConversion {
+    public EquatorialToHorizontalConversion(ZonedDateTime when, GeographicCoordinates where){
+
+    }
+}
