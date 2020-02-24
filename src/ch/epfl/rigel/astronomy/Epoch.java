be03fd4917061ed74c0278f40d3366304658603a
Naarileth <Naarileth@users.noreply.github.com>
2020-02-24 14:13:09 +0100

    Update Epoch.java

--- src/ch/epfl/rigel/astronomy/Epoch.java
+++ src/ch/epfl/rigel/astronomy/Epoch.java
@@ -1,16 +1,31 @@
 package ch.epfl.rigel.astronomy;
 
 import java.time.*;
+import java.time.temporal.ChronoUnit;
 
 public enum Epoch {
-    J2000(LocalDate.of(200, Month.JANUARY, 1)),
-    J2010(LocalDate.of(2010, Month.JANUARY, 1).minusDays(1));
+    J2000(ZonedDateTime.of(
+            LocalDateTime.of(
+                    LocalDate.of(2000, Month.JANUARY, 1),
+                    LocalTime.of(12, 0)),
+            ZoneId.of("UTC"))),
+    J2010(ZonedDateTime.of(
+            LocalDateTime.of(
+                    LocalDate.of(2010, Month.JANUARY, 1).minusDays(1),
+                    LocalTime.of(0, 0)),
+            ZoneId.of("UTC")));
+
+    private ZonedDateTime date;
+
+    Epoch(ZonedDateTime date){
+        this.date = date;
+    }
 
     public double daysUntil(ZonedDateTime when){
-        return 0;
+        return date.until(when, ChronoUnit.MILLIS)/86400000;
     }
 
     public double julianCenturiesUntil(ZonedDateTime when){
-        return 0;
+        return date.until(when, ChronoUnit.MILLIS)/(86400000*36525);
     }
 }
