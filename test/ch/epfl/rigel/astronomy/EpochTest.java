bd7e119bdb87d1c18ca5808e8eab94cb8e53aba9
Naarileth <Naarileth@users.noreply.github.com>
2020-02-24 19:25:53 +0100

    bcp

--- test/ch/epfl/rigel/astronomy/EpochTest.java
+++ test/ch/epfl/rigel/astronomy/EpochTest.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class EpochTest {
 
     @Test
-    void testTrivialEpoch(){
+    void daysUntilWorksOnTrivialEpoch(){
         ZonedDateTime d = ZonedDateTime.of(
                 LocalDate.of(2000, Month.JANUARY, 3),
                 LocalTime.of(18, 0),
--- test/ch/epfl/rigel/coordinates/EclipticToEquatorialConversionTest.java
+++ test/ch/epfl/rigel/coordinates/EclipticToEquatorialConversionTest.java
@@ -10,12 +10,20 @@ import static org.junit.jupiter.api.Assertions.*;
 class EclipticToEquatorialConversionTest {
 
     @Test
-    void EclipticToEquatorialConversion() {
-
-
+    void applyWorksOnNullCoordinates() {
         EclipticToEquatorialConversion etec = new EclipticToEquatorialConversion(ZonedDateTime.now());
-        EquatorialCoordinates ec = etec.apply(EclipticCoordinates.of(Angle.ofHr(2.7625), Angle.ofDMS(34, 54, 66.334765)));
-        assertEquals(0.414118,ec.ra());
+        EquatorialCoordinates conv = etec.apply(EclipticCoordinates.of(Angle.ofHr(0),Angle.ofDMS(0,0,0)));
+        assertEquals(0, conv.ra());
+        assertEquals(0, conv.dec());
     }
+
+    @Test
+    void applyWorks() { //TODO ask for precisions/accuracy
+        EclipticToEquatorialConversion etec = new EclipticToEquatorialConversion(ZonedDateTime.now());
+        EquatorialCoordinates conv = etec.apply(EclipticCoordinates.of(Angle.ofDeg(139.6861111), Angle.ofDeg(4.875277778)));
+        assertEquals(143.7225092, Angle.toDeg(conv.ra()), 1e-3);
+        assertEquals(19.53569924, Angle.toDeg(conv.dec()), 1e-2);
+    }
+
 
 }
