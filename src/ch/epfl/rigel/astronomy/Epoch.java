178b2da3a8cd122db4ba11dbce6483e67960b71f
Naarileth <Naarileth@users.noreply.github.com>
2020-02-24 13:30:57 +0100

    Update Epoch.java

--- src/ch/epfl/rigel/astronomy/Epoch.java
+++ src/ch/epfl/rigel/astronomy/Epoch.java
@@ -1,10 +1,10 @@
 package ch.epfl.rigel.astronomy;
 
-import java.time.ZonedDateTime;
+import java.time.*;
 
 public enum Epoch {
-    J2000,
-    J2010;
+    J2000(LocalDate.of(200, Month.JANUARY, 1)),
+    J2010(LocalDate.of(2010, Month.JANUARY, 1).minusDays(1));
 
     public double daysUntil(ZonedDateTime when){
         return 0;
