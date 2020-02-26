85e3b216fb60cd05d4a4446665fb08a8e009f82c
Naarileth <Naarileth@users.noreply.github.com>
2020-02-26 18:35:14 +0100

    Update EquatorialToHorizontalConversionTest.java

--- test/ch/epfl/rigel/coordinates/EquatorialToHorizontalConversionTest.java
+++ test/ch/epfl/rigel/coordinates/EquatorialToHorizontalConversionTest.java
@@ -8,14 +8,6 @@ import java.time.ZonedDateTime;
 import static org.junit.jupiter.api.Assertions.*;
 
 class EquatorialToHorizontalConversionTest {
-    @Test
-    void applyWorksOnNullCoordinates(){
-        EquatorialToHorizontalConversion ethc = new EquatorialToHorizontalConversion(ZonedDateTime.now(),
-                GeographicCoordinates.ofDeg(0,0));
-        HorizontalCoordinates coord = ethc.apply(EquatorialCoordinates.of(0,0));
-        assertEquals(0, coord.az());
-        assertEquals(0, coord.alt());
-    }
 
     @Test
     void applyWorks(){
