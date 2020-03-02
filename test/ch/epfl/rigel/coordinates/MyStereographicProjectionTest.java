8d6c5cf571dc7992201b491ed93bd2dfba92abec
maxime.zammit <maxime.zammit@epfl.ch>
2020-03-02 19:09:08 +0100

    Update MyStereographicProjectionTest.java

--- test/ch/epfl/rigel/coordinates/MyStereographicProjectionTest.java
+++ test/ch/epfl/rigel/coordinates/MyStereographicProjectionTest.java
@@ -1,7 +1,37 @@
 package ch.epfl.rigel.coordinates;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MyStereographicProjectionTest {
+    @Test
+    void ErrorThrownOnPurpose(){
+        StereographicProjection sp = new StereographicProjection(HorizontalCoordinates.ofDeg(0,0));
+        assertThrows(UnsupportedOperationException.class,()->{
+            sp.hashCode();
+        });
+        assertThrows(UnsupportedOperationException.class,()->{
+            sp.equals(null);
+        });
+    }
+
+    @Test
+    void applyInverseApplyBoomerang(){
+        StereographicProjection sp = new StereographicProjection(HorizontalCoordinates.of(0,0));
+        HorizontalCoordinates hc = HorizontalCoordinates.ofDeg(10,-10);
+        CartesianCoordinates cc = sp.apply(hc);
+        HorizontalCoordinates nhc = sp.inverseApply(cc);
+        assertEquals(hc.azDeg(),nhc.azDeg());
+        assertEquals(hc.altDeg(),nhc.altDeg());
+
+    }
+
+
+
+    @Test
+    void ToStringTest(){
+
+    }
 
 }
