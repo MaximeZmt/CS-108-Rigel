b66d07925a453a4f4c15dc8ff6270e4bfb62af14
maxime.zammit <maxime.zammit@epfl.ch>
2020-03-02 18:41:43 +0100

    UpdateCartesianTest

--- test/ch/epfl/rigel/coordinates/MyCartesianCoordinatesTest.java
+++ test/ch/epfl/rigel/coordinates/MyCartesianCoordinatesTest.java
@@ -2,12 +2,37 @@ package ch.epfl.rigel.coordinates;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Locale;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MyCartesianCoordinatesTest {
+
+    @Test
+    void constructionMethodVerification(){
+        double x = 10987.99987;
+        double y = -9999999.99999;
+        CartesianCoordinates cc = CartesianCoordinates.of(x,y);
+        assertEquals(x,cc.x());
+        assertEquals(y,cc.y());
+    }
+
+    @Test
+    void ErrorThrownOnPurpose(){
+        CartesianCoordinates cc = CartesianCoordinates.of(0,0);
+        assertThrows(UnsupportedOperationException.class,()->{
+           cc.hashCode();
+        });
+        assertThrows(UnsupportedOperationException.class,()->{
+            cc.equals(null);
+        });
+    }
+
     @Test
     void toStringWorks(){
-        CartesianCoordinates cc = CartesianCoordinates.of(23.45372974,0.00002637);
-        System.out.println(cc);
+        double x = 10987.99987;
+        double y = -9999999.99999;
+        CartesianCoordinates cc = CartesianCoordinates.of(x,y);
+        assertEquals(String.format(Locale.ROOT,"(x=%.5f ; y=%.5f)",x,y),cc.toString());
     }
 }
