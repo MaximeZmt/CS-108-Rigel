dbfaf02ff2e3554f4be259198ce9f5f91107f807
Naarileth <Naarileth@users.noreply.github.com>
2020-03-02 19:09:11 +0100

    push

--- test/ch/epfl/rigel/astronomy/MyMoonTest.java
+++ test/ch/epfl/rigel/astronomy/MyMoonTest.java
@@ -12,4 +12,34 @@ class MyMoonTest {
         Moon m = new Moon(EquatorialCoordinates.of(0,0),0,0,0.3752f);
         System.out.println(m.toString());
     }
+
+    @Test
+    void celestialObjectExceptionTestForMoon(){
+        assertThrows(IllegalArgumentException.class,()->{
+            Moon m = new Moon(EquatorialCoordinates.of(0,0),-21,0,0.3752f);
+        });
+        assertThrows(NullPointerException.class,()->{
+            Moon m = new Moon(null,0,0,0.3752f);
+        });
+    }
+
+    @Test
+    void moonFailsOnInvalidPhase(){
+        assertThrows(IllegalArgumentException.class,()->{
+            Moon m = new Moon(EquatorialCoordinates.of(0,0),0,0,-1);
+        });
+    }
+
+    @Test
+    void nameIsLune(){
+        Moon m = new Moon(EquatorialCoordinates.of(0,0),0,0,0);
+        assertEquals("Lune", m.name());
+    }
+
+    @Test
+    void moonWorksOnRandomValues(){
+        Moon m = new Moon(EquatorialCoordinates.of(0,0),152.2628f,0,0.5f);
+        assertEquals(152.2628f,m.angularSize());
+    }
+
 }
--- test/ch/epfl/rigel/astronomy/MySunTest.java
+++ test/ch/epfl/rigel/astronomy/MySunTest.java
@@ -1,7 +1,28 @@
 package ch.epfl.rigel.astronomy;
 
+import ch.epfl.rigel.coordinates.EclipticCoordinates;
+import ch.epfl.rigel.coordinates.EquatorialCoordinates;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MySunTest {
+    @Test
+    void sunFailsOnNullEclipticPosition(){
+        assertThrows(NullPointerException.class,()->{
+            Sun s = new Sun(null, EquatorialCoordinates.of(3.4,1), 34f, 45);
+        });
+    }
 
+    @Test
+    void nameIsSoleil(){
+        Sun s = new Sun(EclipticCoordinates.of(0.4,0.5), EquatorialCoordinates.of(3.4,1), 34f, 45);
+        assertEquals("Soleil", s.name());
+    }
+
+    @Test
+    void magnitudeIsCorrect(){
+        Sun s = new Sun(EclipticCoordinates.of(0.4,0.5), EquatorialCoordinates.of(3.4,1), 34f, 45);
+        assertEquals(-26.7f, s.magnitude());
+    }
 }
