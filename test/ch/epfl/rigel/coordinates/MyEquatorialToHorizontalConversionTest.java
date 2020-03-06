e1e4f61501d10a86ac9234967d487d6ff7faae14
maxime.zammit <maxime.zammit@epfl.ch>
2020-03-06 12:30:55 +0100

    NewTest

--- test/ch/epfl/rigel/coordinates/MyEquatorialToHorizontalConversionTest.java
+++ test/ch/epfl/rigel/coordinates/MyEquatorialToHorizontalConversionTest.java
@@ -15,17 +15,17 @@ class MyEquatorialToHorizontalConversionTest {
 
         //THIS TEST SHOULD BE USED BY CHANGING MANUALLY THE VALUE OF H ( 1.534726189 )
 
-        /*
+
         EquatorialToHorizontalConversion ethc = new EquatorialToHorizontalConversion(ZonedDateTime.now(),
                 GeographicCoordinates.ofDeg(0,52));
         HorizontalCoordinates coord = ethc.apply(EquatorialCoordinates.of(Angle.ofDeg(87.93333),Angle.ofDeg(23.21944)));
-        assertEquals(Angle.ofDeg(283), Angle.toDeg(coord.az()),10e-3);
-        assertEquals(Angle.ofDeg(19), Angle.toDeg(coord.alt()),10e-3);
-
-         */
+        assertEquals(283, coord.azDeg(),10e-3);
+        assertEquals(19, coord.altDeg(),10e-3);
 
 
 
+
+        /*
 
         ZonedDateTime tps1 = ZonedDateTime.of(
                 2020,
@@ -57,6 +57,8 @@ class MyEquatorialToHorizontalConversionTest {
         assertEquals(ethc2.apply(ecRigel).azDeg(),ethc3.apply(ecRigel).azDeg(), 1e-1);
         assertEquals(ethc2.apply(ecRigel).altDeg(),ethc3.apply(ecRigel).altDeg(), 1e-1);
 
+
+         */
 
 
     }
