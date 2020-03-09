728bea578d16d149c79a028e53188169aeae61fa
Naarileth <Naarileth@users.noreply.github.com>
2020-03-09 10:09:09 +0100

    Update EclipticToEquatorialConversion.java

--- src/ch/epfl/rigel/coordinates/EclipticToEquatorialConversion.java
+++ src/ch/epfl/rigel/coordinates/EclipticToEquatorialConversion.java
@@ -40,10 +40,9 @@ public final class EclipticToEquatorialConversion implements Function<EclipticCo
     public EquatorialCoordinates apply(EclipticCoordinates ecl) {
         double lon = ecl.lon();
         double lat = ecl.lat();
-        double ra = Angle.normalizePositive(
-                Math.atan2(Math.sin(lon)*cosEpsilon-Math.tan(lat)*sinEpsilon, Math.cos(lon)));//TODO corrected
+        double ra = Math.atan2(Math.sin(lon)*cosEpsilon-Math.tan(lat)*sinEpsilon, Math.cos(lon));
         double dec = Math.asin(Math.sin(lat)*cosEpsilon+Math.cos(lat)*sinEpsilon*Math.sin(lon));
-        return EquatorialCoordinates.of(ra, dec);
+        return EquatorialCoordinates.of(Angle.normalizePositive(ra), dec);
     }
 
     @Override
