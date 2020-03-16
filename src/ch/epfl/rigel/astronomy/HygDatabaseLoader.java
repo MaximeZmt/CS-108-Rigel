192ce73a006cc93c10c43db380e14126123879ad
maxime.zammit <maxime.zammit@epfl.ch>
2020-03-16 15:22:22 +0100

    Hyg

--- src/ch/epfl/rigel/astronomy/HygDatabaseLoader.java
+++ src/ch/epfl/rigel/astronomy/HygDatabaseLoader.java
@@ -2,11 +2,12 @@ package ch.epfl.rigel.astronomy;
 
 import javafx.util.Builder;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
-import static java.nio.charset.StandardCharsets.US_ASCII;
 
 public enum HygDatabaseLoader implements StarCatalogue.Loader { // public et immuable
     INSTANCE();
@@ -14,6 +15,9 @@ public enum HygDatabaseLoader implements StarCatalogue.Loader { // public et imm
 
     @Override
     public void load(InputStream inputStream, Builder builder) throws IOException {
-        InputStreamReader isr = new InputStreamReader("",US_ASCII);
+        InputStreamReader isr = new InputStreamReader(inputStream, StandardCharsets.US_ASCII);
+        BufferedReader br = new BufferedReader(isr); //TODO Check if this is the correct syntax
+        String s = br.readLine();
+        System.out.println(s);
     }
 }
