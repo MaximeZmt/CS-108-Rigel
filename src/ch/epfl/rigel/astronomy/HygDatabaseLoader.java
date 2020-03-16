db664e779ab54f534e5b66b6f1334b6b099c6a03
maxime.zammit <maxime.zammit@epfl.ch>
2020-03-16 15:01:42 +0100

    UpdateAndAddCatalogs

--- src/ch/epfl/rigel/astronomy/HygDatabaseLoader.java
+++ src/ch/epfl/rigel/astronomy/HygDatabaseLoader.java
@@ -1,4 +1,19 @@
 package ch.epfl.rigel.astronomy;
 
-public enum HygDatabaseLoader {
+import javafx.util.Builder;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import static java.nio.charset.StandardCharsets.US_ASCII;
+
+public enum HygDatabaseLoader implements StarCatalogue.Loader { // public et immuable
+    INSTANCE();
+
+
+    @Override
+    public void load(InputStream inputStream, Builder builder) throws IOException {
+        InputStreamReader isr = new InputStreamReader("",US_ASCII);
+    }
 }
