6345dc973b054bf45cf9746e096442bc7d7a47c9
maxime.zammit <maxime.zammit@epfl.ch>
2020-03-16 13:39:56 +0100

    update

--- src/ch/epfl/rigel/astronomy/MoonModel.java
+++ src/ch/epfl/rigel/astronomy/MoonModel.java
@@ -5,6 +5,7 @@ import ch.epfl.rigel.coordinates.EclipticToEquatorialConversion;
 public class MoonModel implements CelestialObjectModel<Moon> {
     @Override
     public Moon at(double daysSinceJ2010, EclipticToEquatorialConversion eclipticToEquatorialConversion) {
+        //magnitude = 0
         return null;
     }
 }
