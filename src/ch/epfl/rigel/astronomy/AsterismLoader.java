fe9a3fa4dde620d718be7c2ac2c0e9703221793d
maxime.zammit <maxime.zammit@epfl.ch>
2020-03-20 15:28:36 +0100

    AsterismLoader

--- src/ch/epfl/rigel/astronomy/AsterismLoader.java
+++ src/ch/epfl/rigel/astronomy/AsterismLoader.java
@@ -6,9 +6,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-import java.util.stream.Stream;
 
 /**
  * Represents an asterism catalogue loader
--- src/ch/epfl/rigel/astronomy/HygDatabaseLoader.java
+++ src/ch/epfl/rigel/astronomy/HygDatabaseLoader.java
@@ -7,8 +7,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.util.Iterator;
-import java.util.stream.Stream;
+
 
 /**
  * Represents a HYG catalogue loader
