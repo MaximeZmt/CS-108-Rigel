40cd31aa6b4f7e710f26ee8b2c4ac4a216749bf9
Naarileth <Naarileth@users.noreply.github.com>
2020-03-23 15:03:56 +0100

    Update SunModel.java

--- src/ch/epfl/rigel/astronomy/SunModel.java
+++ src/ch/epfl/rigel/astronomy/SunModel.java
@@ -14,19 +14,19 @@ import ch.epfl.rigel.math.Angle;
 public enum SunModel implements CelestialObjectModel<Sun> {
     SUN();
 
-    private final static double theta0 = Angle.ofDeg(0.533128);
-    private final static double e = 0.016705;
-    private final static double sunLongitudeAtJ2010 = Angle.ofDeg(279.557208);
-    private final static double sunLongitudeAtPerigee = Angle.ofDeg(283.112438);
+    private final static double THETA0 = Angle.ofDeg(0.533128);
+    private final static double ECCENTRICITY = 0.016705;
+    private final static double SUN_LONGITUDE_AT_J2010 = Angle.ofDeg(279.557208);
+    private final static double SUN_LONGITUDE_AT_PERIGEE = Angle.ofDeg(283.112438);
 
 
     @Override
     public Sun at(double daysSinceJ2010, EclipticToEquatorialConversion eclipticToEquatorialConversion) {
-        double meanAnomaly = (Angle.TAU/365.242191)*daysSinceJ2010 + sunLongitudeAtJ2010-sunLongitudeAtPerigee;
-        double trueAnomaly = meanAnomaly+2*e*Math.sin(meanAnomaly);
-        double longEcliptique = trueAnomaly + sunLongitudeAtPerigee; // verify if normalize
+        double meanAnomaly = (Angle.TAU/365.242191)*daysSinceJ2010 + SUN_LONGITUDE_AT_J2010 - SUN_LONGITUDE_AT_PERIGEE;
+        double trueAnomaly = meanAnomaly+2* ECCENTRICITY *Math.sin(meanAnomaly);
+        double longEcliptique = trueAnomaly + SUN_LONGITUDE_AT_PERIGEE; // verify if normalize
         double latEcliptique = 0;
-        float angularSize = (float)(theta0 * ((1+e*Math.cos(trueAnomaly))/(1-e*e)));
+        float angularSize = (float)(THETA0 * ((1+ ECCENTRICITY *Math.cos(trueAnomaly))/(1- ECCENTRICITY * ECCENTRICITY)));
         EclipticCoordinates ecliC = EclipticCoordinates.of(Angle.normalizePositive(longEcliptique),latEcliptique);
         EquatorialCoordinates equaC = eclipticToEquatorialConversion.apply(ecliC);
         Sun sunModel = new Sun(ecliC, equaC, angularSize, (float)meanAnomaly);
