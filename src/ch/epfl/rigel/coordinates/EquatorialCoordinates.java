b4691a44ceaefdf98cdbfcb0a55bcfec7a86d2a8
maxime.zammit <maxime.zammit@epfl.ch>
2020-03-27 16:57:41 +0100

    Update EquatorialCoordinates.java

--- src/ch/epfl/rigel/coordinates/EquatorialCoordinates.java
+++ src/ch/epfl/rigel/coordinates/EquatorialCoordinates.java
@@ -16,6 +16,9 @@ import java.util.Locale;
 public final class EquatorialCoordinates extends SphericalCoordinates {
     private final static RightOpenInterval RA_INTERVAL = RightOpenInterval.of(Angle.ofHr(0), Angle.ofHr(24));
     private final static ClosedInterval DEC_INTERVAL = ClosedInterval.of(Angle.ofDeg(-90),Angle.ofDeg(90));
+    /*
+    Intervals that are used to check if the given angle in of() method are valid.
+     */
 
     private EquatorialCoordinates(double rightAscension, double declination) {
         super(rightAscension, declination);
