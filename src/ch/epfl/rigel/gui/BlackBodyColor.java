c32df6ecc5e9c0435b272fd2d744bc3ed61d9998
Naarileth <Naarileth@users.noreply.github.com>
2020-03-30 16:18:28 +0200

    tes

--- src/ch/epfl/rigel/gui/BlackBodyColor.java
+++ src/ch/epfl/rigel/gui/BlackBodyColor.java
@@ -1,5 +1,12 @@
 package ch.epfl.rigel.gui;
 
+import ch.epfl.rigel.Preconditions;
+import ch.epfl.rigel.math.ClosedInterval;
+import javafx.scene.paint.Color;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
 /**
  * [fillTxt]
  *
@@ -7,4 +14,41 @@ package ch.epfl.rigel.gui;
  * @author Maxime Zammit (310251)
  */
 public class BlackBodyColor {
+
+    private BlackBodyColor(){}
+
+    //TODO ask for return statement outside of try catch statement
+    //temp in kelvin
+    public static Color colorForTemperature(double temp) {
+        Preconditions.checkInInterval(ClosedInterval.of(1000,40000),temp);
+
+        temp = temp/100.;
+        temp = Math.round(temp);
+        temp = temp*100;
+        String tempString;
+        if (temp<10000){
+            tempString = " "+temp;
+        } else {
+            tempString = Double.toString(temp);
+        }
+
+        try {
+        //TODO check if correct
+        InputStream colorStream = BlackBodyColor.class.getResourceAsStream("/bbr_color.txt");
+        InputStreamReader isr = new InputStreamReader(colorStream, StandardCharsets.US_ASCII);
+        BufferedReader br = new BufferedReader(isr);
+
+        String line;
+        while ((line = br.readLine())!=null) {
+            if (line.substring(1, 5).equals(tempString)){
+                if (line.charAt(10) == '1'){
+                    return Color.web(line.substring(80,86));
+                }
+            }
+        }
+
+        } catch (IOException e){
+            throw new UncheckedIOException(e);
+        }
+    }
 }
