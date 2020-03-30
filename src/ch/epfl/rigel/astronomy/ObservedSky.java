8a044e81fa19825d8fe329e717bf3f2a6bb9cc78
maxime.zammit <maxime.zammit@epfl.ch>
2020-03-30 13:48:15 +0200

    Update ObservedSky.java

--- src/ch/epfl/rigel/astronomy/ObservedSky.java
+++ src/ch/epfl/rigel/astronomy/ObservedSky.java
@@ -1,10 +1,13 @@
 package ch.epfl.rigel.astronomy;
 
+
 /**
- * [fillTxt]
+ * Represent a set of CelestialObject projected in a plan with a stereographic projection
  *
  * @author Michael Freeman (313215)
  * @author Maxime Zammit (310251)
  */
-public class ObservedSky {
+
+public class ObservedSky { // public et immuable
+
 }
