a8a48d2417f506a4a3f98c14d7124fc08b9372e8
maxime.zammit <maxime.zammit@epfl.ch>
2020-03-30 14:06:37 +0200

    Update ObservedSky.java

--- src/ch/epfl/rigel/astronomy/ObservedSky.java
+++ src/ch/epfl/rigel/astronomy/ObservedSky.java
@@ -1,6 +1,12 @@
 package ch.epfl.rigel.astronomy;
 
 
+import ch.epfl.rigel.coordinates.CartesianCoordinates;
+import ch.epfl.rigel.coordinates.GeographicCoordinates;
+import ch.epfl.rigel.coordinates.StereographicProjection;
+
+import java.time.ZonedDateTime;
+
 /**
  * Represent a set of CelestialObject projected in a plan with a stereographic projection
  *
@@ -9,5 +15,20 @@ package ch.epfl.rigel.astronomy;
  */
 
 public class ObservedSky { // public et immuable
+
+    /*
+    info + model soleil lune planete -> calculer pos projetee dans plan de tout objet celeste (Soleil, Lune,planete, etoile) sauf terre
+     */
+    ObservedSky(ZonedDateTime zdt, GeographicCoordinates observPos, StereographicProjection stereoProj, StarCatalogue starCatalogue){ //public or package-private or private ?
+
+    }
+
+    Sun sun(){
+        //return SunModel.SUN.at();
+        return null
+    }
+    CartesianCoordinates sunPosition(){
+        return null
+    }
 
 }
