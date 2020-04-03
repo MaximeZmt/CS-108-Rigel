ce95d1202f972b6d50126d5c66ce456bc421febd
maxime.zammit <maxime.zammit@epfl.ch>
2020-04-03 15:53:18 +0200

    Update MyAsterismLoaderTest.java

--- test/ch/epfl/rigel/astronomy/MyAsterismLoaderTest.java
+++ test/ch/epfl/rigel/astronomy/MyAsterismLoaderTest.java
@@ -38,14 +38,55 @@ class MyAsterismLoaderTest {
                     //System.out.println(st.toString());
                     assertEquals(List.of("Rigel","Saiph","Alnitak","Betelgeuse","Mu Ori","Xi Ori", "Chi-2 Ori").toString(), st.toString());
                 }
-
-
-
             }
-
         }catch(Exception e){
             //System.out.println(e);
         }
-
     }
+
+
+    @Test
+    void asterismTest2() throws IOException {
+        try{
+            InputStream hygStream = getClass().getResourceAsStream(HYG_CATALOGUE_NAME);
+            InputStream astStream = getClass().getResourceAsStream(ASTERISM);
+            StarCatalogue catalogue = new StarCatalogue.Builder().loadFrom(hygStream, HygDatabaseLoader.INSTANCE).loadFrom(astStream,AsterismLoader.INSTANCE).build();
+            Set<Asterism> ast = catalogue.asterisms();
+            List<Star> starList = catalogue.stars();
+            Iterator i = ast.iterator();
+            while(i.hasNext()){
+                Asterism a = (Asterism) i.next();
+                List<Star> st = a.stars();
+                Iterator ii = st.iterator();
+                Set<Integer> sss = new HashSet<>(List.of(54879,54872,57632,54879,49669,49583,50583,50335,48455,47908));
+
+                while(ii.hasNext()){
+                    Star star = (Star) ii.next();
+                    int id = star.hipparcosId();
+                    if(sss.contains(id)){
+                        sss.remove(id);
+                    }
+                }
+                if (sss.isEmpty()){
+                    System.out.println(st.toString());
+                    System.out.println(catalogue.asterismIndices(a).toString());
+                    List<Integer> ltc = catalogue.asterismIndices(a);
+                    for(Integer num : ltc){
+                        System.out.println(starList.get(num).hipparcosId());
+                    }
+                    //assertEquals(List.of("Rigel","Saiph","Alnitak","Betelgeuse","Mu Ori","Xi Ori", "Chi-2 Ori").toString(), st.toString());
+                }
+            }
+        }catch(Exception e){
+            //System.out.println(e);
+        }
+    }
+
+
+
+
+
+
+
+
 }
