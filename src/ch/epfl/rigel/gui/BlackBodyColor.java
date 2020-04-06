6fc64a235d4112c19259a5fd8907ee78680341d6
Naarileth <Naarileth@users.noreply.github.com>
2020-04-06 15:16:27 +0200

    Update BlackBodyColor.java

--- src/ch/epfl/rigel/gui/BlackBodyColor.java
+++ src/ch/epfl/rigel/gui/BlackBodyColor.java
@@ -1,14 +1,15 @@
 package ch.epfl.rigel.gui;
-/*
+
 import ch.epfl.rigel.Preconditions;
 import ch.epfl.rigel.math.ClosedInterval;
 import javafx.scene.paint.Color;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 
- */
 /**
  * [fillTxt]
  *
@@ -16,44 +17,43 @@ import java.nio.charset.StandardCharsets;
  * @author Maxime Zammit (310251)
  */
 public class BlackBodyColor {
-/*
+
+    private final static String FILE_NAME = "/bbr_color.txt";
+    private final static Map<Integer, Color> TEMP_COLOR_MAP = fileToMap();
+
     private BlackBodyColor(){}
 
     //TODO ask for return statement outside of try catch statement
-    //temp in kelvin
     public static Color colorForTemperature(double temp) {
         Preconditions.checkInInterval(ClosedInterval.of(1000,40000),temp);
 
         temp = temp/100.;
         temp = Math.round(temp);
         temp = temp*100;
-        String tempString;
-        if (temp<10000){
-            tempString = " "+temp;
-        } else {
-            tempString = Double.toString(temp);
-        }
 
-        try {
-        //TODO check if correct
-        InputStream colorStream = BlackBodyColor.class.getResourceAsStream("/bbr_color.txt");
+        return TEMP_COLOR_MAP.get((int)temp);
+    }
+
+    private static Map<Integer, Color> fileToMap(){
+        InputStream colorStream = BlackBodyColor.class.getResourceAsStream(FILE_NAME);
         InputStreamReader isr = new InputStreamReader(colorStream, StandardCharsets.US_ASCII);
         BufferedReader br = new BufferedReader(isr);
 
-        String line;
-        while ((line = br.readLine())!=null) {
-            if (line.substring(1, 5).equals(tempString)){
-                if (line.charAt(10) == '1'){
-                    return Color.web(line.substring(80,86));
+        //TODO ask if better map or list
+        Map<Integer, Color> integerColorMap = new HashMap<>();
+        try {
+            String line;
+            int temperature = 1000;
+            while ((line = br.readLine())!=null) {
+                if ((line.charAt(10) == '1') && (line.charAt(0) != '#')){
+                    integerColorMap.put(temperature, Color.web(line.substring(80,86)));
+                    temperature = temperature+100;
                 }
             }
-        }
 
         } catch (IOException e){
             throw new UncheckedIOException(e);
         }
-        return null;
+        return integerColorMap;
     }
-
- */
 }
