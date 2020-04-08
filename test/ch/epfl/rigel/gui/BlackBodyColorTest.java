af15f091a2d1fc80b73b1255e59e3dc9bf952087
maxime.zammit <maxime.zammit@epfl.ch>
2020-04-08 14:11:26 +0200

    Update BlackBodyColorTest.java

--- test/ch/epfl/rigel/gui/BlackBodyColorTest.java
+++ test/ch/epfl/rigel/gui/BlackBodyColorTest.java
@@ -3,7 +3,7 @@ package ch.epfl.rigel.gui;
 import javafx.scene.paint.Color;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * [description text]
@@ -17,5 +17,19 @@ class BlackBodyColorTest {
     void colorForTemperatureWorksForRandomTemperature(){
         assertEquals(Color.web("#a3c2ff"), BlackBodyColor.colorForTemperature(24100));
         assertEquals(Color.web("#ff932c"), BlackBodyColor.colorForTemperature(2190.546));
+    }
+
+    @Test
+    void onOutIntervalValue(){
+        assertThrows(IllegalArgumentException.class, ()->{
+            BlackBodyColor.colorForTemperature(999);
+        });
+        assertThrows(IllegalArgumentException.class, ()->{
+            BlackBodyColor.colorForTemperature(40001);
+        });
+        assertDoesNotThrow(()->{
+            BlackBodyColor.colorForTemperature(1000);
+            BlackBodyColor.colorForTemperature(40000);
+        });
     }
 }
