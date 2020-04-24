7d9e5f63fcce3462fad22221fe133c9caa2d3d08
Naarileth <Naarileth@users.noreply.github.com>
2020-04-24 15:34:16 +0200

    Update TimeAccelerator.java

--- src/ch/epfl/rigel/gui/TimeAccelerator.java
+++ src/ch/epfl/rigel/gui/TimeAccelerator.java
@@ -11,20 +11,18 @@ import java.time.ZonedDateTime;
  */
 @FunctionalInterface
 public interface TimeAccelerator {
-    /** //TODO do not agree with deltaT var name
+    /**
      * Compute simulate time T
      * @param t0  Initial simulate time
      * @param deltaT Represents (T-T0), the difference of time in nanoseconds
      * @return new ZoneDateTime with the simulate Time T
      */
-    ZonedDateTime adjust(ZonedDateTime t0, long deltaT); //TODO check if public or package private # It is necessarily public cause Functional interface is public -> if private -> should have a body
+    ZonedDateTime adjust(ZonedDateTime t0, long deltaT);
 
-    //TODO check if correct and check if .plusNanos is correct
-    static TimeAccelerator continuous(int alpha){
+    static TimeAccelerator continuous(int alpha){ //TODO possibly type long
         return (t0,deltaT) -> t0.plusNanos(alpha*deltaT);
     }
     
-    //TODO looks shitty
     static TimeAccelerator discrete(long simulatedTimeFrequency, Duration steps){
         return (t0, deltaT) ->
                 t0.plus((steps.multipliedBy(simulatedTimeFrequency*deltaT/1_000_000_000)));
