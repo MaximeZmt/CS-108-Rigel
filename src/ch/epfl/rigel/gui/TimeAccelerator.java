a5fcc95cf80c9076b1510feb120331d7d4283e49
maxime.zammit <maxime.zammit@epfl.ch>
2020-04-24 13:11:16 +0200

    Create TimeAccelerator.java

--- src/ch/epfl/rigel/gui/TimeAccelerator.java
+++ src/ch/epfl/rigel/gui/TimeAccelerator.java
@@ -4,14 +4,20 @@ import java.time.Duration;
 import java.time.ZonedDateTime;
 
 /**
- * [description text]
+ * Represents a time Accelerator, it's compute a simulate time
  *
  * @author Michael Freeman (313215)
  * @author Maxime Zammit (310251)
  */
 @FunctionalInterface
 public interface TimeAccelerator {
-    ZonedDateTime adjust(ZonedDateTime initialSimulatedTime, long realTime); //TODO check if public or package private
+    /** //TODO do not agree with realTime var name
+     * Compute simulate time T
+     * @param t0  Initial simulate time
+     * @param deltaT Represents (T-T0), the difference of time in nanoseconds
+     * @return new ZoneDateTime with the simulate Time T
+     */
+    ZonedDateTime adjust(ZonedDateTime initialSimulatedTime, long realTime); //TODO check if public or package private # It is necessarily public cause Functional interface is public -> if private -> should have a body
 
     //TODO check if correct and check if .plusNanos is correct
     static TimeAccelerator continuous(int alpha){
