ca34fc290e7ca86ae1762192472f9fabbe0dad63
maxime.zammit <maxime.zammit@epfl.ch>
2020-04-24 17:05:48 +0200

    TimeAnimatorT1

--- src/ch/epfl/rigel/gui/TimeAnimator.java
+++ src/ch/epfl/rigel/gui/TimeAnimator.java
@@ -1,6 +1,9 @@
 package ch.epfl.rigel.gui;
 
 import javafx.animation.AnimationTimer;
+import javafx.beans.property.*;
+
+import java.time.ZonedDateTime;
 
 /**
  * [description text]
@@ -9,14 +12,59 @@ import javafx.animation.AnimationTimer;
  * @author Maxime Zammit (310251)
  */
 public final class TimeAnimator extends AnimationTimer {
-    DateTimeBean dateTimeBean;
+    private final DateTimeBean dateTimeBean;
+    private final BooleanProperty running = new SimpleBooleanProperty(false);
+    private final ObjectProperty<TimeAccelerator> timeAccelerator = new SimpleObjectProperty<>(null);
+    private long startedTime = 0;
 
-    TimeAnimator(DateTimeBean dateTimeBean){
+    public TimeAnimator(DateTimeBean dateTimeBean){
         this.dateTimeBean = dateTimeBean;
     }
 
     @Override
     public void handle(long l) {
-
+        if (startedTime == 0){
+            startedTime = l;
+        }else{
+            long deltaT = l-startedTime;
+            startedTime = l;
+            ZonedDateTime zdt = getTimeAccelerator().adjust(dateTimeBean.getZonedDateTime(),deltaT);
+            dateTimeBean.setZonedDateTime(zdt);
+        }
     }
+
+    @Override
+    public void start() {
+        super.start();
+        running.set(true);
+        startedTime = 0;
+    }
+
+    @Override
+    public void stop() {
+        super.stop();
+        running.set(false);
+    }
+
+    public ReadOnlyBooleanProperty runningProperty(){
+        return running;
+    }
+
+    public boolean isRunning(){
+        return running.get();
+    }
+
+    public ObjectProperty<TimeAccelerator> timeAcceleratorProperty(){
+        return timeAccelerator;
+    }
+
+    public TimeAccelerator getTimeAccelerator(){
+        return timeAccelerator.get();
+    }
+
+    public void setTimeAccelerator(TimeAccelerator timeAccelerator){
+        this.timeAccelerator.set(timeAccelerator);
+    }
+
+
 }
