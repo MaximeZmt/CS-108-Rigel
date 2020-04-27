e9b7b502699f3126732b08098ca5caa581afd4f7
maxime.zammit <maxime.zammit@epfl.ch>
2020-04-27 15:29:29 +0200

    Update

--- src/ch/epfl/rigel/gui/SkyCanvasManager.java
+++ src/ch/epfl/rigel/gui/SkyCanvasManager.java
@@ -14,14 +14,33 @@ import javafx.beans.property.SimpleObjectProperty;
  * @author Maxime Zammit (310251)
  */
 public final class SkyCanvasManager {
-    DoubleProperty mouseAzDeg = new SimpleDoubleProperty();
-    DoubleProperty mouseAltDeg = new SimpleDoubleProperty();
-    ObjectProperty<CelestialObject> objectUnderMouse = new SimpleObjectProperty<>();
+    private final DoubleProperty mouseAzDeg = new SimpleDoubleProperty();
+    private final DoubleProperty mouseAltDeg = new SimpleDoubleProperty();
+    private final ObjectProperty<CelestialObject> objectUnderMouse = new SimpleObjectProperty<>();
 
-    SkyCanvasManager(StarCatalogue catalogue,
+    public SkyCanvasManager(StarCatalogue catalogue,
                      DateTimeBean dateTimeBean,
-                     ViewingParametersBean viewingParametersBean,
-                     ObserverLocationBean observerLocationBean){
+                     ObserverLocationBean observerLocationBean,
+                     ViewingParametersBean viewingParametersBean
+                     ){
+/*
+        crée un certain nombre de propriétés et liens décrits plus bas,
+                installe un auditeur (listener) pour être informé des mouvements du curseur de la souris, et stocker sa position dans une propriété,
+        installe un auditeur pour détecter les clics de la souris sur le canevas et en faire alors le destinataire des événements clavier,
+        installe un auditeur pour réagir aux mouvements de la molette de la souris et/ou du trackpad et changer le champ de vue en fonction,
+                installe un auditeur pour réagir aux pressions sur les touches du curseur et changer le centre de projection en fonction,
+                installe des auditeurs pour être informé des changements des liens et propriétés ayant un impact sur le dessin du ciel, et demander dans ce cas au peintre de le redessiner.
+
+        
+ */
 
     }
+
+    /*
+    créer le canevas sur lequel le ciel est dessiné,
+créer le peintre chargé du dessin du ciel et lui faire redessiner le ciel chaque fois que cela est nécessaire,
+changer le champ de vue lorsque l'utilisateur manipule la molette de la souris et/ou le trackpad,
+changer le centre de projection lorsque l'utilisateur appuie sur les touches du curseur,
+suivre les mouvements de la souris et exporter, via des propriétés, la position de son curseur dans le système de coordonnées horizontal, et l'objet céleste le plus proche de ce curseur.
+     */
 }
