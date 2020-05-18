a82694cc53ea7bf3a1ad838756425675ebe4599a
Naarileth <Naarileth@users.noreply.github.com>
2020-05-18 14:58:44 +0200

    swdf

--- src/ch/epfl/rigel/gui/NamedTimeAccelerator.java
+++ src/ch/epfl/rigel/gui/NamedTimeAccelerator.java
@@ -3,7 +3,7 @@ package ch.epfl.rigel.gui;
 import java.time.Duration;
 
 /**
- * [description text]
+ * Represents a named TimeAccelerator
  *
  * @author Michael Freeman (313215)
  * @author Maxime Zammit (310251)
@@ -14,25 +14,44 @@ public enum NamedTimeAccelerator {
     TIMES_300("300x", TimeAccelerator.continuous(300)),
     TIMES_3000("3000×", TimeAccelerator.continuous(3000)),
     DAY("jour", TimeAccelerator.discrete(60, Duration.ofHours(24))),
-    //TODO parse working ?
     SIDERAL_DAY("jour sidéral", TimeAccelerator.discrete(60, Duration.parse("PT23H56M4S")));
 
     private final String name;
     private final TimeAccelerator accelerator;
 
+    /**
+     * Constructor
+     * @param name name
+     * @param accelerator TimeAccelerator
+     */
     NamedTimeAccelerator(String name, TimeAccelerator accelerator){
         this.name = name;
         this.accelerator = accelerator;
     }
 
+    /**
+     * Getter for the name
+     *
+     * @return name
+     */
     public String getName(){
         return name;
     }
 
+    /**
+     * Getter for the accelerator
+     *
+     * @return accelerator
+     */
     public TimeAccelerator getAccelerator(){
         return accelerator;
     }
 
+    /**
+     * @see Object#toString()
+     *
+     * @return name
+     */
     @Override
     public String toString() {
         return name;
