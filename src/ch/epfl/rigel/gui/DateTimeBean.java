ba05aa81ba9d572a184a7472de385921314254ce
Naarileth <Naarileth@users.noreply.github.com>
2020-05-18 14:16:34 +0200

    df

--- src/ch/epfl/rigel/gui/DateTimeBean.java
+++ src/ch/epfl/rigel/gui/DateTimeBean.java
@@ -9,7 +9,7 @@ import java.time.ZoneId;
 import java.time.ZonedDateTime;
 
 /**
- * [fillTxt]
+ * JavaFx bean containing the zone, date and time in wich the observer is
  *
  * @author Michael Freeman (313215)
  * @author Maxime Zammit (310251)
@@ -20,40 +20,100 @@ public final class DateTimeBean {
     private final ObjectProperty<LocalTime> time = new SimpleObjectProperty<>(null);
     private final ObjectProperty<ZoneId> zone = new SimpleObjectProperty<>(null);
 
+    /**
+     * Getter for the date property
+     *
+     * @return date property
+     */
     public ObjectProperty<LocalDate> dateProperty(){
         return date;
     }
+
+    /**
+     * Getter for the date
+     *
+     * @return date
+     */
     public LocalDate getDate(){
         return date.get();
     }
+
+    /**
+     * Setter for the date
+     *
+     * @param date given date
+     */
     public void setDate(LocalDate date){
         this.date.set(date);
     }
 
+    /**
+     * Getter for the time property
+     *
+     * @return time property
+     */
     public ObjectProperty<LocalTime> timeProperty(){
         return time;
     }
+
+    /**
+     * Getter for the time
+     *
+     * @return time
+     */
     public LocalTime getTime(){
         return time.get();
     }
+
+    /**
+     * Setter for the time
+     * @param time given time
+     */
     public void setTime(LocalTime time){
         this.time.set(time);
     }
 
+    /**
+     * Getter for the zone property
+     *
+     * @return zone property
+     */
     public ObjectProperty<ZoneId> zoneProperty(){
         return zone;
     }
+
+    /**
+     * Getter for the zone
+     *
+     * @return zone
+     */
     public ZoneId getZone(){
         return zone.get();
     }
+
+    /**
+     * Setter for the zone
+     *
+     * @param zoneId given zone
+     */
     public void setZone(ZoneId zoneId){
         this.zone.set(zoneId);
     }
 
+    /**
+     * Gets a ZonedDateTime constituted of the zone, date and time of the bean
+     *
+     * @return a ZonedDateTime
+     */
     public ZonedDateTime getZonedDateTime(){
         return ZonedDateTime.of(getDate(),getTime(),getZone());
     }
 
+    /**
+     * Sets the zone, date and time of the bean with the given ZonedDateTime
+     *
+     * @param zdt given ZonedDateTime
+     */
     public void setZonedDateTime(ZonedDateTime zdt){
         setDate(zdt.toLocalDate());
         setTime(zdt.toLocalTime());
